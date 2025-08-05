a71590aa9c0c97f5d941b692c68d3c573229f3f1
priyanka23-byte <priyankaydv317@gmail.com>
2025-08-05 18:20:49 +0530

    Frontend completed

--- youtube-clone/src/Pages/SignUp/SignUp.jsx
+++ youtube-clone/src/Pages/SignUp/SignUp.jsx
@@ -1,20 +1,40 @@
 import React, {useState} from 'react'
 import './SignUp.css'
 import { FaYoutube } from "react-icons/fa";
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import axios from 'axios';
 
 
 const SignUp = () => {
-
-const [signupField , setSignupField] = useState({"channelName":"","userName":"","password":"","about":"","profilePic":""});
-console.log(signupField)
-
 const[uploadedImageUrl,setUploadedImageUrl]= useState("https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?w=360")
+const [signupField , setSignupField] = useState({"channelName":"","userName":"","password":"","about":"","profilePic":uploadedImageUrl});
+
 
 const handleInputField=(event,name)=>{
     setSignupField({
         ...signupField,[name]:event.target.value
     })
+}
+
+console.log(signupField);
+
+const uploadImage= async(e)=>{
+const files = e.target.files;
+
+const data = new FormData();
+data.append('file',files[0]);
+data.append('upload_preset','youtube-clone');
+data.append('cloud_name','dg4wgozjr')
+try{
+  const response = await axios.post(" https://api.cloudinary.com/v1_1/dg4wgozjr/image/upload",data);
+  const imageUrl =response.data.url;
+  setUploadedImageUrl(imageUrl);
+  setSignupField({
+        ...signupField,"profilePic":imageUrl
+    })
+}catch(err){
+    console.log(err);
+}
 }
 
 
@@ -33,7 +53,7 @@ const handleInputField=(event,name)=>{
     <input type="text" className="signup-input" value={signupField.about} onChange={(e)=>{handleInputField(e,"about")}} placeholder=' About your Channel ' />
 
    <div className="signupImg-upload">
-    <input type="file" />
+    <input type="file"onChange={(e)=>uploadImage(e)} />
     <div className="Upload-image">
         <img src={uploadedImageUrl} alt="" className="signupimage" />
     </div>
--- youtube-clone/src/Pages/VideoUpload/VideoUpload.jsx
+++ youtube-clone/src/Pages/VideoUpload/VideoUpload.jsx
@@ -3,10 +3,13 @@ import React ,{useState}from 'react'
 import './VideoUpload.css';
 import { FaYoutube } from "react-icons/fa";
 import {Link} from'react-router-dom';
+import axios from 'axios';
+import { LuLoaderCircle } from "react-icons/lu";
 
 const VideoUpload = () => {
 
 const [inputField, setInputField] =useState({"title":"","description":"","videolink":"","thumbnail":"","videoType":""})
+const [loader, setLoader] =useState(false)
 
 const  handleOnChangeInput=(event,name)=>{
     setInputField({
@@ -14,7 +17,33 @@ const  handleOnChangeInput=(event,name)=>{
      })
    
   }
-  console.log(inputField);
+
+  const uploadImage= async(e,  type)=>{
+    setLoader(true)
+    console.log("Uploading");
+const files = e.target.files;
+
+const data = new FormData();
+data.append('file',files[0]);
+data.append('upload_preset','youtube-clone');
+data.append('cloud_name','dg4wgozjr')
+try{
+  const response = await axios.post(`https://api.cloudinary.com/v1_1/dg4wgozjr/${type}/upload`,data);
+  const url =response.data.url;
+  setLoader(false)
+  let val = type==="image"?"thumbnail":"videolink";
+  setInputField({
+      ...inputField,[val]:url
+     })
+  
+
+}catch(err){
+    setLoader(false)
+    console.log(err);
+}
+}
+
+console.log(inputField);
 
   return (
     <div className='Video-upload'>
@@ -29,8 +58,11 @@ const  handleOnChangeInput=(event,name)=>{
                 <input type="text" placeholder='Description' className="upload-Form-Input"value={inputField.description} onChange={(e)=>{handleOnChangeInput(e,"description")}}/>
                 <input type="text" placeholder='Category' className="upload-Form-Input" value={inputField.videoType} onChange={(e)=>{handleOnChangeInput(e,"videoType")}}/>
 
-                <div>Thumbnail <input type="file"  accept="image/*"/></div>
-                <div>Video <input type="file"  accept="video/mp4"/></div>
+                <div>Thumbnail <input type="file"  accept="image/*" onChange={(e)=>uploadImage(e, "image")}/></div>
+                <div>Video <input type="file"  accept="video/mp4 , video/webm, video/* "onChange={(e)=>uploadImage(e, "video")}/></div>
+               {
+                 loader && <LuLoaderCircle  className='loader'/>
+               }
             </div>
             <div className="uploadBtns">
                 <div className="uploadbtn-form">Upload</div>
